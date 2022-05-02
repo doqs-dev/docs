60522926c505ad68efd44c86d42c8f1608626832
dnnsthnnr <dennis.thanner+gh@lemon.markets>
2022-05-02 13:54:11 +0200

    feat: first pages

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from './layout.module.css'
 
 // @ts-ignore
 export default function Layout({children}) {
@@ -12,20 +13,22 @@ export default function Layout({children}) {
           <aside className="flex-none sticky top-0 overflow-y-auto px-5 h-screen border-slate-100">
 
             <div id="sidebar-header" className="my-3">
-              <img src="logo.svg" width={20} alt="Logo" className={"inline-block"}/>
+              <img src="/logo.svg" width={20} alt="Logo" className={"inline-block"}/>
               <span className={"inline-block pl-2 text-sm"}>doqs</span>
             </div>
 
-            <ul>
-              <li>
+            <ul className={"text-sm"}>
+              <li className={styles.li}>
                 <a href="/">Introduction</a>
               </li>
-              <li>API Reference</li>
+              <li className={"uppercase text-gray-500 text-sm my-3"}>API Reference</li>
+              <li className={styles.li}><a href="/api-reference/authentication">Authentication</a></li>
+              <li className={styles.li}><a href="/api-reference/create-template">Create Template</a></li>
             </ul>
 
           </aside>
           <div className={"flex-1 divide-y-2"}>
-            <main className="ml-8 py-8">
+            <main className="ml-8 py-8 mr-8">
               {children}
             </main>
             <footer>
