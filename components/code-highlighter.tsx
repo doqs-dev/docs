10165292e52dc45a0a6c82a12bd6bfb2c9864d53
dnnsthnnr <dennis.thanner+gh@lemon.markets>
2022-05-02 21:40:33 +0200

    fix: build

--- components/code-highlighter.tsx
+++ components/code-highlighter.tsx
@@ -1,22 +1,23 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 import darcula from 'react-syntax-highlighter/dist/cjs/styles/hljs/darcula';
-import { PropsWithChildren } from "react";
 
 type SimpleCodeHighlighterProps = {
   title?: string;
   language?: string;
+  code: string | string[];
 };
 
 export const SimpleCodeHighlighter = ({
                                         title,
                                         language = 'JSON',
-                                        children,
-                                      }: PropsWithChildren<SimpleCodeHighlighterProps>) => {
+                                        code
+                                      }: SimpleCodeHighlighterProps) => {
+  // @ts-ignore
   return (
       <div className="code-highlighter">
         {title && <h3>{title}</h3>}
         <SyntaxHighlighter language={language} style={darcula} wrapLines wrapLongLines>
-          {children}
+          {code}
         </SyntaxHighlighter>
       </div>
   );
