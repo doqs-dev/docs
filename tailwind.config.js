e397c7a4972666be4555e02255f56ca15f300bbb
dnnsthnnr <dennis.thanner+gh@lemon.markets>
2022-05-02 11:14:02 +0200

    feat: dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx,md}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+function withOpacityValue(variable) {
+    return ({opacityValue}) => {
+        if (opacityValue === undefined) {
+            return `rgb(var(${variable}))`;
+        }
+        return `rgb(var(${variable}) / ${opacityValue})`;
+    };
 }
+
+module.exports = {
+    content: ['./src/**/*.html'],
+    theme: {
+        extend: {
+            colors: {
+                // allow tailwind opacity operator on color. need to be specified in main scss file
+                primary: withOpacityValue('--color-primary'),
+                secondary: withOpacityValue('--color-secondary'),
+            },
+        },
+        fontFamily: {
+            sans: ['Roboto', 'sans-serif'],
+        },
+        container: {
+            center: true,
+            // default horizontal padding
+            padding: '1rem',
+        },
+    },
+    plugins: [],
+    darkMode: 'class',
+};
