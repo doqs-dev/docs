f07d8c737e42fac431565d1ae13e2849f2073869
dnnsthnnr <dennis.thanner+gh@lemon.markets>
2022-05-03 20:20:29 +0200

    feat: simple badge

---
+++ components/badge.tsx
@@ -0,0 +1,34 @@
+import { PropsWithChildren } from "react";
+
+type BadgeType = 'success' | 'warning' | 'error' | 'info'
+
+type BadgeProps = {
+  type: BadgeType
+};
+
+export const Badge = ({children, type}: PropsWithChildren<BadgeProps>) => {
+
+  let className = ''
+
+  switch (type) {
+    case 'success':
+      className = 'bg-green'
+      break;
+    case 'warning':
+      className = 'bg-yellow'
+      break;
+    case 'error':
+      className = 'bg-red-600 text-white'
+      break;
+    case 'info':
+      className = 'bg-blue'
+      break;
+  }
+
+
+  return (
+      <span className={className + " py-2 px-4 rounded"}>
+        {children}
+      </span>
+  );
+};
