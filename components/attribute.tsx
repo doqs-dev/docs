4a9d35cdb96cc7cfa97b48963ccf600e983465cc
dnnsthnnr <dennis.thanner+gh@lemon.markets>
2022-05-04 22:00:58 +0200

    feat: required and optional tag

--- components/attribute.tsx
+++ components/attribute.tsx
@@ -11,8 +11,9 @@ export const Attribute = ({name, required, children}: PropsWithChildren<Attribut
 
       <div className="border-t-2 py-3">
         <div>
-          <span className="inline-block bg-gray-700 text-white p-0.5 rounded text-sm">{name}</span>
-          <span></span>
+          <span className="inline-block bg-gray-700 text-white p-0.5 rounded text-sm mr-2">{name}</span>
+          { required && <span className="text-xs text-red-900">required</span> }
+          { !required && <span className="text-xs">optional</span> }
         </div>
         {children}
       </div>
