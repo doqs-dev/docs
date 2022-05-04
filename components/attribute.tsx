835b6f5ccc0c20c98fdba58e208b33a93271ee31
dnnsthnnr <dennis.thanner+gh@lemon.markets>
2022-05-04 22:11:54 +0200

    fix: styling

--- components/attribute.tsx
+++ components/attribute.tsx
@@ -10,7 +10,7 @@ export const Attribute = ({name, required, children}: PropsWithChildren<Attribut
   return (
 
       <div className="border-t-2 py-2">
-        <div>
+        <div className="mb-1">
           <span className="inline-block bg-gray-700 text-white py-0.5 px-1 rounded text-sm mr-2">{name}</span>
           { required && <span className="text-xs text-red-900">required</span> }
           { (!required && required != null) && <span className="text-xs">optional</span> }
