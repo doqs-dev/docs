668c227f35ef60572f83c28dd90da35e005fd2fb
Dennis <dennis.thanner+gh@lemon.markets>
2022-05-06 11:45:16 +0200

    feat: badge sizing (#12)
    * feat: add size param
    
    * chore: badge
    
    * feat: badge in menu

--- components/badge.tsx
+++ components/badge.tsx
@@ -3,31 +3,46 @@ import { PropsWithChildren } from "react";
 export type BadgeType = 'success' | 'warning' | 'error' | 'info'
 
 type BadgeProps = {
-  type: BadgeType
+  type: BadgeType,
+  size?: 'sm' | 'md'
 };
 
-export const Badge = ({children, type}: PropsWithChildren<BadgeProps>) => {
+export const Badge = ({children, type, size}: PropsWithChildren<BadgeProps>) => {
+
+  if (!size) {
+    size = 'md'
+  }
+
 
   let className = ''
 
   switch (type) {
     case 'success':
-      className = 'bg-green-400 text-white'
+      className = 'bg-green-400 '
       break;
     case 'warning':
-      className = 'bg-amber-500 text-white'
+      className = 'bg-amber-500'
       break;
     case 'error':
-      className = 'bg-red-500 text-white'
+      className = 'bg-red-500'
       break;
     case 'info':
-      className = 'bg-blue-400 text-white'
+      className = 'bg-blue-400'
       break;
   }
 
+  let sizeClass = '';
+  switch (size) {
+    case "md":
+      sizeClass = 'px-3 py-1';
+      break;
+    case "sm":
+      sizeClass = 'px-1.5 py-0.5'
+      break;
+  }
 
   return (
-      <span className={className + " py-1 px-3 rounded"}>
+      <span className={`${className} ${sizeClass} text-white rounded`}>
         {children}
       </span>
   );
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from './layout.module.css'
+import { Badge } from "./badge";
 
 // @ts-ignore
 export default function Layout({children}) {
@@ -23,8 +24,14 @@ export default function Layout({children}) {
               </li>
               <li className={"uppercase text-gray-500 text-sm my-3"}>API Reference</li>
               <li className={styles.li}><a href="/api-reference/authentication">Authentication</a></li>
-              <li className={styles.li}><a href="/api-reference/create-template">Create template</a></li>
-              <li className={styles.li}><a href="/api-reference/fill-template">Fill template</a></li>
+              <li className={styles.li}><a href="/api-reference/create-template">
+                <small className="inline-block mr-0.5"><Badge type="success" size="sm">POST</Badge></small>
+                Create template
+              </a></li>
+              <li className={styles.li}><a href="/api-reference/fill-template">
+                <small className="inline-block mr-0.5"><Badge type="success" size="sm">POST</Badge></small>
+                Fill template
+              </a></li>
             </ul>
 
           </aside>
