dbd2c6ece8059f919bfd11c08840e7a60ee7e94c
Dennis <dennis.thanner+gh@lemon.markets>
2022-05-18 15:27:04 +0200

    feat: splitbee tracking (#17)
    * feat: cold
    
    * feat: load theme
    
    * feat: custom base colors
    
    * feat: yello and orange colors
    
    * feat: yello and orange colors
    
    * fix: ci
    
    * fix: vars
    
    * feat: link to imprint
    
    * feat: split bee integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout';
+import Head from "next/head";
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <>
+        <Head>
+          <script async data-no-cookie data-token="W948WND3QU8F" src="https://cdn.splitbee.io/sb.js"></script>
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
   );
 }
 
