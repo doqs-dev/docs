3d29be7335be7ed0a4fafd692c98495411c2ed88
dnnsthnnr <dennis.thanner+gh@lemon.markets>
2022-05-27 13:45:07 +0200

    feat: base structure

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Sitemap = () => {
+};
+
+export default Sitemap;
+
+// @ts-ignore
+export const getServerSideProps = ({res}) => {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    </urlset>
+  `;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
