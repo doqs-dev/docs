648af0983375dd882f4d5d4c73818551d0c03795
dnnsthnnr <dennis.thanner+gh@lemon.markets>
2022-05-29 18:47:58 +0200

    feat: api key

--- components/request-example.tsx
+++ components/request-example.tsx
@@ -5,7 +5,7 @@ import { useCallback, useState, SyntheticEvent } from "react";
 type Props = {
   method: 'post' | 'get' | 'put' | 'delete',
   url: string;
-  headers: object | null;
+  headers: object;
   file: { key: string, fileName: string, variable: string } | null;
   json: object | null;
 }
@@ -23,6 +23,7 @@ export const RequestExample = (props: Props) => {
     console.debug('handleChange', val);
     setValue(val.currentTarget.value as Language);
   };
+
   const codeMap: { [key: string]: string } = {
     Python: `
 import requests
@@ -36,8 +37,12 @@ resp = requests.post("${baseUrl}${props.url}",
         ) : ''
     }
 )
-`.trimStart()
-  }
+`.trimStart(),
+    cURL: `curl --request ${props.method} \\
+--url "${baseUrl}${props.url}" \\
+${ Object.entries(props.headers).map(val =>  `-H "${val[0]}: ${val[1]}"` ).join(' \\ \n') } \\
+${props.json? `-d '${JSON.stringify(props.json)}'` : ''}
+`}
 
   console.log(codeMap[value], value);
   return (
@@ -56,3 +61,14 @@ resp = requests.post("${baseUrl}${props.url}",
   )
 
 };
+
+/* curl --request POST \
+  --url https://api.doqs.dev/v1/templates/%7Bid%7D/fill \
+  --header 'Content-Type: application/json' \
+  --header 'x-api-key: your-api-key' \
+  --data '{
+	"data": {}
+}'
+*
+*
+* */
