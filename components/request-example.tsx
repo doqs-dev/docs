44eead5766d8d51c770d7d50c724653a19401129
dnnsthnnr <dennis.thanner+gh@lemon.markets>
2022-05-29 19:55:59 +0200

    feat: copy to clip board

--- components/request-example.tsx
+++ components/request-example.tsx
@@ -49,15 +49,26 @@ ${
 `
   }
 
+  const copyToClipboard = useCallback(() => {
+    navigator.clipboard.writeText(codeMap[value]);
+  }, [value, codeMap]);
+
   console.log(codeMap[value], value);
   return (
       <div>
-        <div className={'py-1.5 bg-gray-900 text-right'}>
+        <div className={'py-1.5 bg-gray-900 flex'}>
           <select className={'appearance-none bg-transparent text-gray-200 px-2 mr-2 outline-none'}
                   onChange={handleChange}>
             {LANGUAGES.map(lang => <option key={lang}
                                            value={lang}>{lang}</option>)}
           </select>
+          <button className={'text-white'} title={'Copy to clipboard'} onClick={copyToClipboard}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-auto w-4" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M8 2a1 1 0 000 2h2a1 1 0 100-2H8z"/>
+              <path
+                  d="M3 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v6h-4.586l1.293-1.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L10.414 13H15v3a2 2 0 01-2 2H5a2 2 0 01-2-2V5zM15 11h2a1 1 0 110 2h-2v-2z"/>
+            </svg>
+          </button>
         </div>
         <div>
           {value && (<SimpleCodeHighlighter language={value.toString().toLowerCase()} code={codeMap[value]}/>)}
