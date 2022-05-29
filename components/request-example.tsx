79705218c55aa81e32727fd136e5a515af3e4b74
dnnsthnnr <dennis.thanner+gh@lemon.markets>
2022-05-29 18:00:20 +0200

    feat: select field

--- components/request-example.tsx
+++ components/request-example.tsx
@@ -1,5 +1,6 @@
 import { SimpleCodeHighlighter } from "./code-highlighter";
 import { baseUrl } from "./constants";
+import { useCallback, useState } from "react";
 
 type Props = {
   method: 'post' | 'get' | 'put' | 'delete',
@@ -9,26 +10,45 @@ type Props = {
   json: object | null;
 }
 
+
+type Language = 'Python' | 'cURL';
+const LANGUAGES: Array<Language> = ['Python', 'cURL'];
+
+
 export const RequestExample = (props: Props) => {
 
+  const [value, setValue] = useState<Language>('Python')
+
+  const handleChange = useCallback((val: Language) => {
+    setValue(val);
+  }, []);
+
   return (
-      <>
-        <SimpleCodeHighlighter language="python"
-                               code={`
+      <div>
+        <div className={'py-1.5 bg-gray-900 text-right'}>
+          <select className={'appearance-none bg-transparent text-gray-200 px-2 mr-2 outline-none'} onChange={handleChange}>
+            {LANGUAGES.map(lang => <option key={lang}
+                                           value={lang}>{lang}</option>)}
+          </select>
+        </div>
+        <div>
+          <SimpleCodeHighlighter language="python"
+                                 code={`
 import requests
 ${props.file ? `${props.file.variable} = open('${props.file.fileName}', 'rb')` : ''}
 resp = requests.post("${baseUrl}${props.url}",
     headers=${JSON.stringify(props.headers)},
     ${props.file || props.json ?
-                                   (
-                                       (props.file ? `files={"${props.file?.key}": (${props.file?.fileName}, ${props.file?.variable})},` : '') +
-                                       (props.json ? `json=${JSON.stringify(props.json)},` : '')
-                                   ) : ''
-                               }
+                                     (
+                                         (props.file ? `files={"${props.file?.key}": (${props.file?.fileName}, ${props.file?.variable})},` : '') +
+                                         (props.json ? `json=${JSON.stringify(props.json)},` : '')
+                                     ) : ''
+                                 }
 )
 `.trimStart()}
-        />
-      </>
+          />
+        </div>
+      </div>
   )
 
 };
