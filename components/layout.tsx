64d12effbc6179d5802da4d7542f50f88c331553
Dennis <16762453+dnnsthnnr@users.noreply.github.com>
2023-05-19 17:08:27 +0200

    feat: document pdf generator (#55)
    * chore: restructure
    
    * chore: restructure
    
    * feat: more space in the bottom
    
    * feat: document render capability
    
    * feat: document restrictions

--- components/layout.tsx
+++ components/layout.tsx
@@ -26,22 +26,29 @@ export default function Layout({children}) {
               <li className={styles.li}>
                 <a href="/">Introduction</a>
               </li>
-              <li className={"uppercase text-gray-500 text-sm my-3"}>API Reference</li>
-              <li className={styles.li}><a href="/api-reference/authentication">Authentication</a></li>
-              <li className={styles.li}><a href="/api-reference/create-template">
+              <li className={styles.li}><a href="/authentication">Authentication</a></li>
+
+              <li className={"uppercase text-gray-500 text-sm my-3"}>PDF Filling</li>
+              <li className={styles.li}><a href="/pdf-filling/create-template">
                 <small className="inline-block mr-0.5"><Badge type="success" size="sm">POST</Badge></small>
                 Create template
               </a></li>
-              <li className={styles.li}><a href="/api-reference/fill-template">
+              <li className={styles.li}><a href="/pdf-filling/fill-template">
                 <small className="inline-block mr-0.5"><Badge type="success" size="sm">POST</Badge></small>
                 Fill template
               </a></li>
 
-              <li className={"uppercase text-gray-500 text-sm my-3"}>Fields</li>
-              <li className={styles.li}><a href="/fields/text">Text field</a></li>
-              <li className={styles.li}><a href="/fields/checkbox">Checkbox field</a></li>
-              <li className={styles.li}><a href="/fields/date">Date field</a></li>
-              <li className={styles.li}><a href="/fields/image">Image field</a></li>
+              <li className={"uppercase text-gray-500 text-xs my-1"}>Fields</li>
+              <li className={styles.li}><a href="/pdf-filling/fields/text">Text field</a></li>
+              <li className={styles.li}><a href="/pdf-filling/fields/checkbox">Checkbox field</a></li>
+              <li className={styles.li}><a href="/pdf-filling/fields/date">Date field</a></li>
+              <li className={styles.li}><a href="/pdf-filling/fields/image">Image field</a></li>
+
+              <li className={"uppercase text-gray-500 text-sm my-3"}>PDF Generator</li>
+              <li className={styles.li}><a href="/pdf-generator/render">
+                <small className="inline-block mr-0.5"><Badge type="success" size="sm">POST</Badge></small>
+                Render document
+              </a></li>
             </ul>
 
           </aside>
@@ -50,7 +57,7 @@ export default function Layout({children}) {
               {children}
             </main>
             <footer>
-              <div className="ml-8 mt-5 text-gray-700 grid grid-flow-col sm:auto-cols-max">
+              <div className="ml-8 mt-5 pb-2 text-gray-700 grid grid-flow-col sm:auto-cols-max">
                 <span className="mr-10 font-bold">
                 <a href={"https://doqs.dev"}>doqs.dev</a>
               </span>
